651195c1f1c8dabee0c8f466e306cd7980df463b
Tag Locklar <tlocklar3@gmail.com>
2024-09-03 12:40:37 -0600

    Wonderland Software
    its alive!!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState, useMemo } from "react";
+import "./App.css";
+import rabbitSprite from "./assets/rabbit_sprite.png";
 
 function App() {
+  const [frame, setFrame] = useState(0);
+  const requestRef = useRef();
+  const previousTimeRef = useRef();
+
+  const animate = (time) => {
+    if (previousTimeRef.current != undefined) {
+      const deltaTime = time - previousTimeRef.current;
+
+      // Update frame every 150ms
+      if (deltaTime > 150) {
+        setFrame((prevFrame) => (prevFrame + 1) % 24);
+        previousTimeRef.current = time;
+      }
+    } else {
+      previousTimeRef.current = time;
+    }
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []);
+
+  const spriteStyle = useMemo(
+    () => ({
+      backgroundImage: `url(${rabbitSprite})`,
+      backgroundPosition: `${-(frame % 4) * 64}px ${
+        -Math.floor(frame / 4) * 64
+      }px`,
+    }),
+    [frame]
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>wonderland.software</h1>
+        <div className="rabbit-container">
+          <div className="rabbit-sprite" style={spriteStyle} />
+        </div>
       </header>
     </div>
   );
